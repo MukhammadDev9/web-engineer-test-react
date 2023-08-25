c4812efaad5621cebad4f417b64be965009744ea
Mukhammad <mukhammaddev9@gmail.com>
2023-08-25 13:52:19 +0500

    initial style settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 function App() {
-    return <div className="App">Hello, world!</div>
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <main className="content">
+                    <div className="content__container">
+                        <p>Hello, world!</p>
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
 }
 
 export default App
