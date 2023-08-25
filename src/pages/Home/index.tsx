525a842e2ca897ca4028ebba8109cb9bc51cd776
Mukhammad <mukhammaddev9@gmail.com>
2023-08-25 22:03:10 +0500

    finish home page and some navbar changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,41 @@
 import { FC } from "react"
 import { useLoad } from "../../hooks/request"
+import { THomeMetaResponse } from "./types"
 
 interface Props {}
 
 const Home: FC<Props> = ({}) => {
-    const { response, loading } = useLoad({ url: "home.json" })
+    const getRequest = useLoad({ url: "home.json" })
+    const { response, loading }: THomeMetaResponse = getRequest
 
     return (
         <section className="home-page">
             <div className="home-page__container">
-                <h1 className="home-page--title page-">{!loading && response?.meta?.title}</h1>
-                <p className="home-page__description">{!loading && response?.meta?.description}</p>
-                <div className="quotes">
-                    
+                <h1 className="home-page--title page-">{!loading && response.meta?.title}</h1>
+                <p className="home-page__description">{!loading && response.meta?.description}</p>
+                <div className="topic">
+                    <ul className="topic__list">
+                        {!loading &&
+                            response.meta?.topics.map((topic) => (
+                                <li className="topic__item">
+                                    <h2 className="topic__title">{topic.topic_title}</h2>
+                                    <div className="quote">
+                                        <ul className="quote__list">
+                                            {topic.quotes.map((quote) => (
+                                                <li className="quote__item">
+                                                    <h3 className="quote__title">
+                                                        {quote.quote_title}
+                                                    </h3>
+                                                    <p className="quote__body">
+                                                        {quote.quote_body}
+                                                    </p>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </li>
+                            ))}
+                    </ul>
                 </div>
             </div>
         </section>
--- src/pages/Home/types.ts
+++ src/pages/Home/types.ts
@@ -1,8 +1,23 @@
-export type THomeMetaResponse = {
+type NumberID = number
+
+export interface THomeMetaResponse {
+    loading: boolean
     response: {
         name: string
         meta: {
             title: string
+            description: string
+            topics: TTopics[]
         }
     }
+}
+
+type TTopics = NumberID & {
+    topic_title: string
+    quotes: TQuotes[]
+}
+
+type TQuotes = NumberID & {
+    quote_title: string
+    quote_body: string
 }
